b5e799c58038855e2b28261ec759a4482172d271
Alain D'Ettorre <alain.det@gmail.com>
2020-04-11 14:25:16 +0200

    Add basic inline styling to learn-jsx app

--- courses/modern-react-with-redux/learn-jsx/src/index.js
+++ courses/modern-react-with-redux/learn-jsx/src/index.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-  <div>Hello world!</div>
-);
+const getButtonText = () => 'Submit';
+
+const MyForm = () => {
+
+  const style = {
+    all: {
+      fontFamily: 'Trebuchet MS',
+    },
+    label: {
+      margin: '0 0.5rem',
+    },
+    input: {
+      margin: '0 0.5rem',
+      padding: '0.5rem 1rem',
+      border: '1px solid #ccc',
+      borderRadius: '0.5rem',
+    },
+    button: {
+      backgroundColor: '#008',
+      color: 'white',
+      padding: '0.5rem 1rem',
+      border: 'none',
+      borderRadius: '0.5rem',
+    }
+  };
+
+  return (
+    <div style={style.all}>
+      <label style={style.label}>Enter name:</label>
+      <input style={style.input} id="name" type="text" />
+      <button style={style.button} type="submit">
+        {getButtonText()}
+      </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <MyForm />
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
