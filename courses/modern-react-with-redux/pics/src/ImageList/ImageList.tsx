ef134f4a6e473e8bdbe6afda823506ac316d60b5
Alain D'Ettorre <alain.det@gmail.com>
2020-04-13 09:18:40 +0200

    Add <ImageList>

---
+++ courses/modern-react-with-redux/pics/src/ImageList/ImageList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ImageList.scss';
+import ImageListProps from './ImageListProps.interface';
+
+export default function ImageList(props: ImageListProps) {
+  return (
+    <div>
+      Image List
+    </div>
+  );
+};
