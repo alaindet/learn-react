7b3290060d645d2122eee69cf90898141bffe432
Alain D'Ettorre <alain.det@gmail.com>
2020-04-13 09:18:48 +0200

    Add <SearchBar>

---
+++ courses/modern-react-with-redux/pics/src/SearchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SearchBar.scss';
+import SearchBarProps from './SearchBarProps.interface';
+
+export default function SearchBar(props: SearchBarProps) {
+  return (
+    <div>
+      Search Bar
+    </div>
+  );
+};
