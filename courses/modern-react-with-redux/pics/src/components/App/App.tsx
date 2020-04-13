cab363ad5b31aae5acbbc16897c0f7fdb0016a4a
Alain D'Ettorre <alain.det@gmail.com>
2020-04-13 09:44:57 +0200

    Add logic to <SearchBar>

--- courses/modern-react-with-redux/pics/src/components/App/App.tsx
+++ courses/modern-react-with-redux/pics/src/components/App/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+
 import './App.scss';
+import SearchBar from '../SearchBar/SearchBar';
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div className="ui container" style={{ marginTop: '10px' }}>
+      <SearchBar />
+    </div>
   );
 }
 
--- courses/modern-react-with-redux/pics/src/components/SearchBar/SearchBar.tsx
+++ courses/modern-react-with-redux/pics/src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
 import './SearchBar.scss';
-import SearchBarProps from './SearchBarProps.interface';
 
-export default function SearchBar(props: SearchBarProps) {
-  return (
-    <div>
-      Search Bar
-    </div>
-  );
-};
+export default class SearchBar extends React.Component {
+
+  state = {
+    term: '',
+  };
+
+  private onInputChange(event: React.FormEvent<HTMLInputElement>): void {
+    const term = event.currentTarget.value;
+    this.setState({ term });
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="image-search">Image Search</label>
+            <input
+              type="text"
+              id="image-search"
+              placeholder="Type to search..."
+              value={this.state.term}
+              onChange={this.onInputChange.bind(this)}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
