cf2d00ae7ffb422f594801ef40c1f0c4bbc53914
Alain D'Ettorre <alain.det@gmail.com>
2020-04-14 09:31:34 +0200

    Put fetching logic into <App />

--- courses/modern-react-with-redux/pics/src/components/App/App.tsx
+++ courses/modern-react-with-redux/pics/src/components/App/App.tsx
@@ -4,11 +4,21 @@ import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 import './App.scss';
 import SearchBar from './../SearchBar/SearchBar';
-import SearchPhotosResponse from './../../interfaces/SearchPhotoResponse';
+import { SearchPhotosImage, SearchPhotosResponse } from './../../interfaces/SearchPhotoResponse';
 
-export default class App extends React.Component {
+interface Props {}
 
-  public onSearchSubmit(term: string): void {
+interface State {
+  images: SearchPhotosImage[];
+}
+
+export default class App extends React.Component<Props, State> {
+
+  state: State = {
+    images: []
+  };
+
+  private async onSearchSubmit(term: string) {
 
     const url = ''+process.env.REACT_APP_API_SEARCH_PHOTOS_URL;
     const accessKey = process.env.REACT_APP_API_ACCESS_KEY;
@@ -21,15 +31,24 @@ export default class App extends React.Component {
       }
     };
 
-    axios.get(url, config).then((response: AxiosResponse<SearchPhotosResponse>) => {
-      console.log('response', response);
-    });
+    let response: AxiosResponse<SearchPhotosResponse>;
+    response = await axios.get(url, config);
+    this.setState({ images: response.data.results });
+  }
+
+  private renderResultsCounter(): JSX.Element | null {
+    if (!this.state.images.length) {
+      return null;
+    }
+
+    return <p>Found {this.state.images.length} images</p>;
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit.bind(this)} />
+        {this.renderResultsCounter()}
       </div>
     );
   }
