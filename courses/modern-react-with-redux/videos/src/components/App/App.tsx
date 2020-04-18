f7550933b5cc30459f93c8bdc89877610e2992ff
Alain D'Ettorre <alain.det@gmail.com>
2020-04-18 09:30:46 +0200

    Add 'videos' app

---
+++ courses/modern-react-with-redux/videos/src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'components/App/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      Videos app
+    </div>
+  );
+}
+
+export default App;
