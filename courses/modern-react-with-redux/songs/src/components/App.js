eaf9153cc4c18065ab2c016ef5d77ec6a593ca1d
Alain D'Ettorre <alain.det@gmail.com>
2020-04-19 01:43:17 +0200

    Add first use of connect react-redux

--- courses/modern-react-with-redux/songs/src/components/App.js
+++ courses/modern-react-with-redux/songs/src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import SongList from './SongList';
-import { selectSong } from './../actions';
 
 const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column four wide">
+          Song Details
+        </div>
+      </div>
     </div>
   );
 };
--- courses/modern-react-with-redux/songs/src/components/SongList.js
+++ courses/modern-react-with-redux/songs/src/components/SongList.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class SongList extends Component {
+import { selectSong } from './../actions';
+
+class SongList extends Component {
+
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div key={song.title} className="item">
+          <div className="right floated content">
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary"
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>
-        SongList
+      <div className="ui divided list">
+        {this.renderList()}  
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    songs: state.songs,
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
