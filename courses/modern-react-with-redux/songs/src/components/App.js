aa20de2b19a794a7816902a332fc225a57de49ef
Alain D'Ettorre <alain.det@gmail.com>
2020-04-19 01:16:21 +0200

    Add base for <SongList>

--- courses/modern-react-with-redux/songs/src/components/App.js
+++ courses/modern-react-with-redux/songs/src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import SongList from './SongList';
 import { selectSong } from './../actions';
 
 const App = () => {
   return (
     <div>
-      Songs app
+      <SongList />
     </div>
   );
 };
