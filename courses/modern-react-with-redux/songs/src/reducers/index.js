f81b2187a5e75c66cb2a1ca38edd38ef94910679
Alain D'Ettorre <alain.det@gmail.com>
2020-04-19 01:11:38 +0200

    Add actions and reducers for Redux

---
+++ courses/modern-react-with-redux/songs/src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+
+export const songsReducer = () => {
+  return [
+    { title: 'Never Gonna Give You Up', year: 1987 },
+    { title: 'Together Forever', year: 1987 },
+    { title: 'She Wants to Dance with Me', year: 1988 },
+    { title: 'Whenever You Need Somebody', year: 1987 },
+  ];
+};
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
