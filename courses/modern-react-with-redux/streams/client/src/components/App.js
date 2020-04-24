97019c115460a36f07b43773743a0788a8d2512e
Alain D'Ettorre <alain.det@gmail.com>
2020-04-24 09:12:28 +0200

    Add scaffolding and basic routing

--- courses/modern-react-with-redux/streams/client/src/components/App.js
+++ courses/modern-react-with-redux/streams/client/src/components/App.js
@@ -1,33 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click me</button>
-    </div>
-  );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import NotFound from './errors/NotFound';
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pageTwo" component={PageTwo} />
-        </div>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/show" component={StreamShow} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
