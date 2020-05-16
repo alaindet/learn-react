49480e5ccdda698946da33d9bf60c79ee6ed95f1
Alain D'Ettorre <alain.det@gmail.com>
2020-05-16 10:44:10 +0200

    Refactor GoogleAuth methods names

--- courses/modern-react-with-redux/streams/client/src/components/GoogleAuth.js
+++ courses/modern-react-with-redux/streams/client/src/components/GoogleAuth.js
@@ -35,11 +35,11 @@ class GoogleAuth extends React.Component {
     }
   }
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   }
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -50,7 +50,7 @@ class GoogleAuth extends React.Component {
 
     if (!this.state.isSignedIn) {
       return (
-        <button className="ui red google button" onClick={this.onSignIn}>
+        <button className="ui red google button" onClick={this.onSignInClick}>
           <i className="google icon" />
           Sign In with Google
         </button>
@@ -58,7 +58,7 @@ class GoogleAuth extends React.Component {
     }
 
     return (
-      <button className="ui red google button" onClick={this.onSignOut}>
+      <button className="ui red google button" onClick={this.onSignOutClick}>
         <i className="google icon" />
         Sign Out
       </button>
