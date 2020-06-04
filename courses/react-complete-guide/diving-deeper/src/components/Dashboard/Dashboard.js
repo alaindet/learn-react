d49a01906ce1e9cf32265039976cd683b7a3c842
Alain D'Ettorre <alain.dettorre@avanade.com>
2020-06-04 17:45:07 +0200

    Add base dashboard component

---
+++ courses/react-complete-guide/diving-deeper/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { DashboardStyled } from './Dashboard.style';
+
+const Dashboard = () => (
+  <DashboardStyled>
+    Dashboard
+  </DashboardStyled>
+);
+
+export default Dashboard;
---
+++ courses/react-complete-guide/diving-deeper/src/components/Dashboard/Dashboard.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const DashboardStyled = styled.div`
+  margin: 1rem 0;
+  padding: 1rem 0;
+  padding-bottom: 1.5rem;
+  margin-bottom: 2rem;
+  border-bottom: 2px solid #adf;
+`;
