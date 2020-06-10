e8cc801dc251f018ec70f12f280c19a68c802dc8
Alain D'Ettorre <alain.dettorre@avanade.com>
2020-06-10 18:33:59 +0200

    Add burger-builder-basic/

---
+++ courses/react-complete-guide/burger-builder-basic/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Burger Builder
+      </div>
+    );
+  }
+}
+
+export default App;
