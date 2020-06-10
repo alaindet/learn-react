e3a0740c7919cde00b367e9f0addc8d16d7edffb
Alain D'Ettorre <alain.dettorre@avanade.com>
2020-06-10 18:50:54 +0200

    Add <Layout> base component

--- courses/react-complete-guide/burger-builder-basic/src/App.js
+++ courses/react-complete-guide/burger-builder-basic/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Burger Builder
+        <Layout>
+          Burger Builder
+        </Layout>
       </div>
     );
   }
