1d07e6610ed1f7c0fed7400420b8acf4a20a0022
Alain D'Ettorre <alain.dettorre@avanade.com>
2020-06-17 18:35:35 +0200

    Fix <ButtonGroup>

--- courses/react-complete-guide/burger-builder-basic/src/components/UI/Button/ButtonGroup/ButtonGroup.style.js
+++ courses/react-complete-guide/burger-builder-basic/src/components/UI/Button/ButtonGroup/ButtonGroup.style.js
@@ -1,13 +1,22 @@
 import styled from 'styled-components';
 
 import theme from 'theme/theme';
+import { ButtonStyled } from 'components/UI/Button/Button.style';
 
 export const ButtonGroupStyled = styled.div`
   display: flex;
   justify-content: space-between;
   flex-direction: column;
 
+  > ${ButtonStyled} {
+    display: block;
+  }
+
   @media (min-width: ${theme.breakpoint.mobileToTablet}px) {
     flex-direction: row;
+
+    > ${ButtonStyled} {
+      display: initial;
+    }
   }
 `;
