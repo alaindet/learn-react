026dd0dd5967d926d3cf1f37d5d7fe827dd2bed0
Alain D'Ettorre <alain.det@gmail.com>
2021-12-17 18:01:53 +0100

    Add project boilerplate for Battling Tanks

---
+++ projects/battling-tanks/src/App.tsx
@@ -0,0 +1,10 @@
+import { Tank } from './common/components/Tank/Tank';
+import './App.scss';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Tank />
+    </div>
+  );
+};
---
+++ projects/battling-tanks/src/common/components/Tank/Tank.tsx
@@ -0,0 +1,11 @@
+import './Tank.scss';
+
+export const Tank = () => {
+  return (
+    <div className="tank">
+      <div className="tank-head-1"></div>
+      <div className="tank-head-2"></div>
+      <div className="tank-head-3"></div>
+    </div>
+  );
+};
