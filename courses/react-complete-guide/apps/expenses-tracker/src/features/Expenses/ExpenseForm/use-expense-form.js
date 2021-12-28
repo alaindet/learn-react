6db5b7a14c7dc444b2e8e12c8131937840dd0141
Alain D'Ettorre <alain.det@gmail.com>
2021-12-28 10:19:09 +0100

    Update form hook with useState instead of useReducer

--- courses/react-complete-guide/apps/expenses-tracker/src/features/Expenses/ExpenseForm/use-expense-form.js
+++ courses/react-complete-guide/apps/expenses-tracker/src/features/Expenses/ExpenseForm/use-expense-form.js
@@ -1,53 +1,45 @@
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { formatDate } from '../../../utils';
 
 export const useExpenseForm = (expense, formSchema) => {
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'all':
-
-        // Updating whole form
-        if (action.payload) {
-          const { title, amount, date: rawDate } = action.payload;
-          const date = formatDate('y-m-d', rawDate);
-          return { title, amount, date };
-        }
-
-        // Resetting
-        return {
-          title: '',
-          amount: 0,
-          date: formatDate('y-m-d', Date.now()),
-        };
-
-      // Updating single field
-      default:
-        const { type, payload } = action;
-        return { ...state, [type]: payload };
-    }
-  };
 
   const initialState = {
+    id: expense?.id ?? '',
     title: expense?.title ?? '',
     amount: expense?.amount ?? 0,
     date: formatDate('y-m-d', expense?.date ?? Date.now()),
   };
 
-  const [state, dispatch] = useReducer(reducer, initialState);
   const [isValid, setIsValid] = useState(false);
+  const [formValue, setFormValue] = useState(initialState);
 
   useEffect(() => {
-    dispatch({ type: 'all', payload: expense });
+
+    // Updating whole form
+    if (expense) {
+      const { id, title, amount, date: rawDate } = expense;
+      const date = formatDate('y-m-d', rawDate);
+      setFormValue({ id, title, amount, date });
+      return;
+    }
+
+    // Resetting
+    setFormValue({
+      title: '',
+      amount: 0,
+      date: formatDate('y-m-d', Date.now()),
+    });
+
   }, [expense]);
 
-  const validateForm = (formValue) => {
+  const validateForm = (formToValidate) => {
     for (const field of Object.keys(formSchema)) {
       for (const validator of formSchema[field]) {
         switch(validator) {
           case 'required':
             // Note: this makes 0 invalid
-            if (!formValue[field]) {
+            if (!formToValidate[field]) {
               setIsValid(false);
               return;
             }
@@ -62,10 +54,10 @@ export const useExpenseForm = (expense, formSchema) => {
   };
 
   const updateForm = (field, value) => {
-    dispatch({ type: field, payload: value });
-    const toValidate = { ...state, [field]: value };
+    setFormValue(prevState => ({ ...prevState, [field]: value }));
+    const toValidate = { ...formValue, [field]: value };
     validateForm(toValidate);
   };
 
-  return [state, updateForm, isValid];
+  return [formValue, updateForm, isValid];
 };
