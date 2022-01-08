f0f0340a0a63aaa0b2e6160e5c77e44c6766b9ee
Alain D'Ettorre <alain.det@gmail.com>
2022-01-08 09:39:12 +0100

    Add code-splitting tutorial to follow

--- tutorials/code-splitting/src/core/store/store.ts
+++ tutorials/code-splitting/src/core/store/store.ts
@@ -3,6 +3,7 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import commonReducer from './common.slice';
 import aboutReducer from 'src/features/about/store';
 
+// TODO: https://redux.js.org/usage/code-splitting
 export const store = configureStore({
   devTools: process.env.NODE_ENV !== 'production',
   reducer: {
