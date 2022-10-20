63499871adf64e003e46247c08da9aac7de44ed3
Alain D'Ettorre <alain.det@gmail.com>
2022-10-20 13:54:51 +0200

    Add first describe() grouping

--- courses/testing-react-jest-rtl/color-button/src/App.js
+++ courses/testing-react-jest-rtl/color-button/src/App.js
@@ -6,24 +6,40 @@ function getOppositeColor(color) {
   return color === 'red' ? 'blue' : 'red';
 }
 
+export function addSpacesToCamelCase(camel) {
+  return camel.replace(/[A-Z]/g, ' $&').trim();
+}
+
 export function App() {
 
   const [color, setColor] = useState('red');
-  const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState(true);
   const nextColor = useMemo(() => getOppositeColor(color), [color]);
   const flipColor = () => setColor(nextColor);
 
   return (
     <div className="app">
-      <button type="button" style={{backgroundColor: color}} onClick={flipColor}>
+
+      {/* The button */}
+      <button
+        id="the-button"
+        type="button"
+        className={`-${color}`}
+        onClick={flipColor}
+        disabled={!enabled}
+      >
         Change to {nextColor}
       </button>
+
+      {/* The checkbox */}
       <input
         type="checkbox"
+        id="button-activation"
         name="button-activation"
-        value={enabled}
+        checked={enabled}
         onChange={e => setEnabled(e.target.checked)}
       />
+      <label htmlFor="button-activation">Enabled?</label>
     </div>
   );
 }
