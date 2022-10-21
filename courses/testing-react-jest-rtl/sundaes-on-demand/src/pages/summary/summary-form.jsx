9bcf55d717e983c34c1385ade9cc953312697f7b
Alain D'Ettorre <alain.det@gmail.com>
2022-10-21 17:58:04 +0200

    WIP

--- courses/testing-react-jest-rtl/sundaes-on-demand/src/pages/summary/summary-form.jsx
+++ courses/testing-react-jest-rtl/sundaes-on-demand/src/pages/summary/summary-form.jsx
@@ -1,31 +1,26 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 export function SummaryForm() {
 
   const [termsAccepted, setTermsAccepted] = useState(false);
 
-  const checkboxLabel = (
-    <span>
-      I agree to
-      <span style={{ color: 'blue' }}> Terms and Conditions</span>
-    </span>
-  );
-
   return (
-    <Form>
-      <Form.Group controlId="terms-and-conditions">
-        <Form.Check
+    <form>
+      <div className="form-control">
+        <input
           type="checkbox"
+          id="terms-and-conditions"
           checked={termsAccepted}
           onChange={e => setTermsAccepted(e.target.checked)}
-          label={checkboxLabel}
         />
-      </Form.Group>
-      <Button variant="primary" type="submit" disabled={!termsAccepted}>
-        Confirm order
-      </Button>
-    </Form>
+        <label htmlFor="terms-and-conditions">
+          I agree to
+          <span style={{ color: 'blue' }}> Terms and Conditions</span>
+        </label>
+        <button type="submit" disabled={!termsAccepted}>
+          Confirm order
+        </button>
+      </div>
+    </form>
   );
 }
