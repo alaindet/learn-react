753668b0d194d0de8f9e6f01b4174f00213910eb
Alain D'Ettorre <alain.det@gmail.com>
2022-10-21 09:43:25 +0200

    WIP

---
+++ courses/testing-react-jest-rtl/sundaes-on-demand/src/pages/summary/order-summary.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SummaryForm } from './summary-form';
+
+export function OrderSummary() {
+  return (
+    <div className="order-summary">
+      <h1>Order Summary</h1>
+      <SummaryForm />
+    </div>
+  );
+}
---
+++ courses/testing-react-jest-rtl/sundaes-on-demand/src/pages/summary/summary-form.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import { SummaryForm } from './summary-form';
+
+const TERMS_NAME = /i agree to terms and conditions/i;
+const CONFIRM_NAME=  /confirm order/i;
+
+// describe('SummaryForm component', () => {
+
+//   test('checkbox is unchecked by default', () => {
+//     render(<SummaryForm />);
+//     const termsCheckbox = screen.getByRole('checkbox', { name: TERMS_NAME });
+//     expect(termsCheckbox).not.toBeChecked();
+//   });
+
+//   test('checkbox enables/disables button when clicked', () => {
+//     render(<SummaryForm />);
+//     const termsCheckbox = screen.getByRole('checkbox', { name: TERMS_NAME });
+//     const confirmButton = screen.getByRole('button', { name: CONFIRM_NAME });
+//     expect(confirmButton).toBeDisabled();
+//     fireEvent.click(termsCheckbox);
+//     expect(confirmButton).toBeEnabled();
+//     fireEvent.click(termsCheckbox);
+//     expect(confirmButton).toBeDisabled();
+//   });
+// });
