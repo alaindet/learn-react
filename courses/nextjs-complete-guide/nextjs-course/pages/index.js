6f37074681794ae1e43de2c1c54601d7847b1c8e
Alain D'Ettorre <alain.det@gmail.com>
2022-10-23 08:48:40 +0200

    Reset project to course basic example

--- courses/nextjs-complete-guide/nextjs-course/pages/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>This is going to be great</h1>
-    </div>
-  );
-}
