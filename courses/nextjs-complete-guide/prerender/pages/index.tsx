02f53e89848a33bd79256f49d7ee34434dfaa896
Alain D'Ettorre <alain.det@gmail.com>
2022-10-25 01:01:04 +0200

    Add example for data fetching on pre-rendering

--- courses/nextjs-complete-guide/prerender/pages/index.tsx
+++ courses/nextjs-complete-guide/prerender/pages/index.tsx
@@ -1,10 +1,38 @@
-export default function HomePage() {
+import fs from 'fs/promises';
+import path from 'path';
+
+// NOTE: Current working directory is the root path when executing Next.js
+// Not the /pages directory
+async function loadDataFromFileSystem(): Promise<HomePageProps> {
+  const root = process.cwd();
+  const filePath = path.join(root, 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath, { encoding: 'utf-8' });
+  return JSON.parse(jsonData);
+}
+
+// getStaticProps() gets called first, if present
+// This is only executed on the server side
+// Any imported file used exclusively here is removed from the frontend bundle
+// (ex.: 'fs/promises' and 'path' native modules)
+export async function getStaticProps(): Promise<{ props: HomePageProps }> {
+  const props = await loadDataFromFileSystem();
+  return { props };
+}
+
+interface Product {
+  id: string;
+  title: string;
+}
+
+interface HomePageProps {
+  products: Product[];
+}
+
+export default function HomePage({ products }: HomePageProps) {
   return (
     <div>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {products.map(p => <li key={p.id}>{p.title}</li>)}
       </ul>
     </div>
   )
