3c9186d35ca9495d46fe3be49de01c3ae6b9f0c4
Alain D'Ettorre <alain.det@gmail.com>
2022-10-24 10:41:15 +0200

    Setup event detail page

--- courses/nextjs-complete-guide/events-app/pages/events/[event-id].tsx
+++ courses/nextjs-complete-guide/events-app/pages/events/[event-id].tsx
@@ -1,9 +1,20 @@
 import { useRouter } from 'next/router';
+import { getEventById } from '../../mock-data';
 
 export default function EventDetailPage() {
 
   const router = useRouter();
   const eventId = router.query['event-id'];
+  const event = getEventById(eventId as string);
+
+  // TODO: Redirect?
+  if (!event) {
+    return (
+      <p>No event found with id <strong>{eventId}</strong></p>
+    );
+  }
+
+  console.log(event);
 
   return (
     <>
