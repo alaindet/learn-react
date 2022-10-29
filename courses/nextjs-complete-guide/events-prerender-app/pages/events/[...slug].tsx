6f06bcbd87f1b69d77cacf3bf5cfe1f602edbf40
Alain D'Ettorre <alain.det@gmail.com>
2022-10-29 11:16:19 +0200

    Add SSR to [...slug].tsx of prerendered events app

--- courses/nextjs-complete-guide/events-prerender-app/pages/events/[...slug].tsx
+++ courses/nextjs-complete-guide/events-prerender-app/pages/events/[...slug].tsx
@@ -1,73 +1,76 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 import { EventList } from '../../components/events/event-list/event-list';
 import * as api from '../../api';
 import { EventFilters, LiveEvent } from '../../types';
+import { GetServerSidePropsContext } from 'next';
 
-function isInputValid(input: any): boolean {
-  if (!input) return false;
-  if (input?.length !== 2) return false;
+// TODO: Move elsewhere?
+const helpers = {
+  isInputValid: (input: any): boolean => {
+    if (!input) return false;
+    if (input?.length !== 2) return false;
 
-  const year = +input[0];
-  if (isNaN(year) || year < 2000 || year > 3000) return false;
+    const year = +input[0];
+    if (isNaN(year) || year < 2000 || year > 3000) return false;
 
-  const month = +input[1];
-  if (isNaN(month) || month <= 1 || month >= 12) return false;
+    const month = +input[1];
+    if (isNaN(month) || month <= 1 || month >= 12) return false;
 
-  return true;
+    return true;
+  },
+  parseInput: (input: any): EventFilters => {
+    const [year, month] = input.map((val: any) => +val);
+    return { year, month };
+  },
+};
+
+interface FilteredEventsPageProps {
+  validInput: boolean;
+  filters: EventFilters | null;
+  results: LiveEvent[];
 }
 
-function parseInput(input: any): EventFilters {
-  const [year, month] = input.map((val: any) => +val);
-  return { year, month };
-}
+export default function FilteredEventsPage({
+  validInput,
+  filters,
+  results,
+}: FilteredEventsPageProps) {
 
-export default function FilteredEventsPage() {
-
-  const router = useRouter();
-
-  if (!isInputValid(router.query['slug'])) {
+  if (!validInput) {
     return (
       <div className="content-container">
         <h1>Error</h1>
-        <p>No valid filters</p>
+        <p>No valid filters provided</p>
       </div>
     );
   }
 
-  const filters = parseInput(router.query['slug']);
-  const events: LiveEvent[] = [];
-  // const events = getFilteredEvents(filters);
-
-  let result: JSX.Element;
-
-  if (events.length) {
-    result = <EventList events={events} />;
-  } else {
-    result = <p>No events, go back to <Link href="/events">all events</Link></p>;
-  }
+  const resultsView = !results.length
+    ? <p>No events found, go back to <Link href="/events">all events</Link></p>
+    : <EventList events={results} />; 
 
   return (
     <div className="content-container">
-      <h1>Events happening on {filters.year}/{filters.month}</h1>
-      {result}
+      <h1>Events happening on {filters?.year}/{filters?.month}</h1>
+      {resultsView}
     </div>
   );
 }
 
-// export async function getStaticProps(context: GetStaticPropsContext) {
-//   const id = context.params!['event-id'];
-//   if (!id) return { notFound: true };
-//   const event: LiveEvent | null = await api.getEventById(+id);
-//   if (!event) return { notFound: true };
-//   const props = { event } as EventDetailPageProps;
-//   return { props };
-// }
+export async function getServerSideProps(context: GetServerSidePropsContext) {
 
-// export async function getStaticPaths() {
-//   const events = await api.getAllEvents();
-//   const paths = events.map(ev => ({ params: { 'event-id': ev.id }}));
-//   const fallback = 'blocking';
-//   return { fallback, paths };
-// }
+  if (!helpers.isInputValid(context.query['slug'])) {
+    const validInput = false;
+    const filters = null;
+    const results: LiveEvent[] = [];
+    const props = { validInput, filters, results } as FilteredEventsPageProps;
+    return { props };
+  }
+
+  const validInput = true;
+  const filters = helpers.parseInput(context.query['slug']);
+  const results = await api.getFilteredEvents(filters.year, filters.month);
+  const props = { validInput, filters, results } as FilteredEventsPageProps;
+  return { props };
+};
