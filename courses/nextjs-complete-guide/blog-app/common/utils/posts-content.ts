cd2afb5e717e071a872dfe9895512397e24015ca
Alain D'Ettorre <alain.det@gmail.com>
2022-11-06 19:39:59 +0100

    Remove mock data

--- courses/nextjs-complete-guide/blog-app/common/utils/posts-content.ts
+++ courses/nextjs-complete-guide/blog-app/common/utils/posts-content.ts
@@ -1,9 +1,14 @@
-import { readAllPosts } from "./posts-persistence";
+import { FullBlogPost } from '../types';
+import { readAllPosts, readPost } from './posts-persistence';
 
-export function getPosts() {
+export function getPosts(): FullBlogPost[] {
   return readAllPosts();
 }
 
-export function getFeaturedPosts() {
+export function getFeaturedPosts(): FullBlogPost[] {
   return getPosts().filter((post) => post.isFeatured);
+}
+
+export function getPost(slug: string): FullBlogPost | null {
+  return readPost(`${slug}.md`);
 }
--- courses/nextjs-complete-guide/blog-app/pages/posts/[postslug].tsx
+++ courses/nextjs-complete-guide/blog-app/pages/posts/[postslug].tsx
@@ -1,8 +1,29 @@
-import { MOCK_POST } from '@/mock';
-import { PostContent } from '@/common/components/posts';
+import { GetStaticPropsContext } from 'next';
 
-export default function PostPage() {
-  return (
-    <PostContent post={MOCK_POST} />
-  );
+import { PostContent } from '@/common/components/posts';
+import { getFeaturedPosts, getPost } from '@/common/utils';
+import { FullBlogPost } from '@/common/types';
+
+interface PostPageProps {
+  post: FullBlogPost;
+}
+
+export default function PostPage({ post }: PostPageProps) {
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps(context: GetStaticPropsContext) {
+  const slug = context.params!['postslug'];
+  if (!slug || Array.isArray(slug)) return { notFound: true };
+  const post: FullBlogPost | null = getPost(slug);
+  if (!post) return { notFound: true };
+  const props = { post } as PostPageProps;
+  const ONE_HOUR = 60 * 60;
+  return { props, revalidate: ONE_HOUR };
+}
+
+export function getStaticPaths() {
+  const featuredPosts = getFeaturedPosts();
+  const paths = featuredPosts.map(post => ({ params: { 'postslug': post.slug } }));
+  return { paths, fallback: 'blocking' };
 }
