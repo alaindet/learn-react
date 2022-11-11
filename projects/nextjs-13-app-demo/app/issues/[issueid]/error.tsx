f59d325655cc7174ff66c0b5e4eed1717d45b559
Alain D'Ettorre <alain.det@gmail.com>
2022-11-11 14:13:25 +0100

    Fix Next.js 13 preview app

--- projects/nextjs-13-app-demo/app/issues/[issueid]/error.tsx
+++ projects/nextjs-13-app-demo/app/issues/[issueid]/error.tsx
@@ -1,4 +1,7 @@
-export default function IssueDetailsErrorPage() {
+'use client';
+
+export default function IssueDetailsErrorPage({ error }: any) {
+  console.log('################################### ERROR', error);
   const issueId = 42; // TODO
   return <p>No issue with ID {issueId}</p>
 }
